8f2d43a80ab0d2957f24d36c5119167ae1943989
Vitório A. Cavalheiro <vitoriocavalheiro03@gmail.com>
2023-09-14 13:12:41 -0300

    feat: Add support for choosing between generic and custom salt
    I've added support for choosing between using a generic (default) salt or providing a custom salt when generating tokens. This increases the flexibility of the project by allowing users to control the salt used in token generation.
    
    Details of the changes:
    - Added explanation about the concept of salt to the README.
    - Documented the use of the "useGenericSalt" parameter and the "customSalt" field in the "How to Run the Project" section of the README.
    
    Signed-off-by: Vitório A. Cavalherio <vitoriocavalheiro03@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+"""
+This module contains a Flask application that handles a POST request to '/generate_token'. It receives data from the frontend, including the character count, whether to use a generic salt, and a custom salt. It then uses the GenerateToken class to generate a random token with the specified character count and salt value. The generated token, salt, and SHA value are returned as a JSON response.
+
+Example Usage:
+    ```python
+    # POST request to '/generate_token'
+    data = {
+        'characterCount': 12,
+        'useGenericSalt': True,
+        'customSalt': 'abc123'
+    }
+    response = client.post('/generate_token', json=data)
+    print(response.json())
+    # Output: {'token': 'aBcDeFgHiJkL', 'salt': 'abc123', 'sha': '...'}
+    ```
+
+Attributes:
+    app (Flask): The Flask application instance.
+
+Routes:
+    - '/' (GET): Renders the index.html template.
+    - '/generate_token' (POST): Handles the generation of a custom token.
+
+"""
+
 from flask import Flask, render_template, request, jsonify, make_response
 from backend.gen_token import GenerateToken
 
@@ -5,25 +30,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Renders the index.html template.
+
+    Returns:
+        str: The rendered HTML template.
+    """
     return render_template('index.html')
 
 @app.route('/generate_token', methods=['POST'])
 def generate_custom_token():
-    # Obtenha os dados do formulário enviado pelo frontend
-    data = request.json
-    # Obtenha os valores específicos do dicionário de dados
-    quantidade_caracteres = int(data.get('quantidadeCaracteres', 10))
-    usar_salt_generico = data.get('usarSaltGenerico')
-    salt_personalizado = data.get('saltPersonalizado', '')
-    token_generator = GenerateToken()
-    token, salt, sha = token_generator.generate_token(quantidade_caracteres, usar_salt_generico, salt_personalizado)
+    """
+    Handles the generation of a custom token.
 
-    # Crie um objeto de resposta JSON
+    Returns:
+        Response: The Flask response object containing the generated token, salt, and SHA value.
+    """
+    # Get data from the form submitted by the frontend
+    data = request.json
+    # Get specific values from the data dictionary
+    character_count = int(data.get('characterCount', 10))
+    use_generic_salt = data.get('useGenericSalt')
+    custom_salt = data.get('customSalt', '')
+    token_generator = GenerateToken()
+    token, salt, sha = token_generator.generate_token(character_count, use_generic_salt, custom_salt)
+    print(sha)
+
+    # Create a JSON response object
     response_data = {'token': token, 'salt': salt, 'sha': sha}
 
-    # Crie e retorne uma resposta Flask
+    # Create and return a Flask response
     response = make_response(jsonify(response_data))
-    response.status_code = 200  # Código de status HTTP 200 (OK)
+    response.status_code = 200  # HTTP status code 200 (OK)
 
     return response
 
--- backend/gen_token.py
+++ backend/gen_token.py
@@ -5,29 +5,56 @@ import bcrypt
 class GenerateToken:
     @staticmethod
     def generate_salt():
+        """
+        Generate a random salt value.
+
+        Returns:
+            str: The generated salt value.
+        """
         characters = string.ascii_letters + string.digits
-        salt_generico = ''.join(secrets.choice(characters) for _ in range(20))
-        return salt_generico
+        generic_salt = ''.join(secrets.choice(characters) for _ in range(20))
+        return generic_salt
     
     @staticmethod
-    def generate_token(quantidade_caracteres=10, usar_salt_generico=True, salt_personalizado=""):
+    def generate_token(character_count=10, use_generic_salt=True, custom_salt=""):
+        """
+        Generate a random token with a specified character count and salt value.
+
+        Args:
+            character_count (int, optional): The number of characters in the token. Defaults to 10.
+            use_generic_salt (bool, optional): Whether to use a generic salt value. Defaults to True.
+            custom_salt (str, optional): A custom salt value to use. Defaults to "".
+
+        Returns:
+            tuple: A tuple containing the generated token, salt, and SHA value.
+        """
         characters = string.ascii_letters + string.digits
-        token = ''.join(secrets.choice(characters) for _ in range(quantidade_caracteres))
+        token = ''.join(secrets.choice(characters) for _ in range(character_count))
         
-        if usar_salt_generico:
+        if use_generic_salt:
             salt = GenerateToken.generate_salt()
         else:
-            salt = salt_personalizado
+            salt = custom_salt
         
-        # Verifique se token e salt não estão vazios antes de chamar generate_sha
+        # Check if token and salt are not empty before calling generate_sha
         if token and salt:
             sha = GenerateToken.generate_sha(token, salt)
             return token, salt, sha
         else:
-            return None, None, None  # Tratar o caso em que token ou salt é vazio
+            return None, None, None  # Handle the case where token or salt is empty
 
     @staticmethod
     def generate_sha(token, salt):
+        """
+        Generate a secure hash algorithm (SHA) value for a given token and salt.
+
+        Args:
+            token (str): The token to generate the SHA value for.
+            salt (str): The salt value to use in the SHA generation.
+
+        Returns:
+            str: The generated SHA value.
+        """
         return str(bcrypt.kdf(
             password=token.encode('utf-8'),
             salt=salt.encode('utf-8'),
